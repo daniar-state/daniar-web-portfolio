7b2ec5f3780605b7ed1f95ecd449db1a61f3cee7
daniar-state <150630975+daniar-state@users.noreply.github.com>
2023-12-03 21:36:04 +0600

    Updating project

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -1,10 +1,10 @@
 export const projects = [
     {
-        title: "Realtime Chat App",
-        desc: " Информация о приложение и шаблоне проекта №1",
-        stacks: ["Next.JS", "Express", "API", "Socket.IO"],
-        image: "/next.svg",
-        sourceCode: "https://github.com/",
-        liveAt: "https://github.com/",
+        title: "Сервис API Интеграция с Tilda",
+        desc: " Tilda Integration Suite - это мощный скрипт на языке Python, предназначенный для автоматизации процессов продаж на платформе Tilda. Этот инструмент интегрирует Tilda с передовыми платежными и аналитическими сервисами VIPayment, Moogold и JollyMax, обеспечивая бесперебойную работу и повышая эффективность продаж.",
+        stacks: ["Python", "API", "Flask", "Tilda", "SQLAlchemy", "SQLite", "Git"],
+        image: "/tildasuite.svg",
+        sourceCode: "https://github.com/daniar-state/Tilda-Integration-Suite",
+        liveAt: "https://github.com/daniar-state/Tilda-Integration-Suite",
     } 
 ]
